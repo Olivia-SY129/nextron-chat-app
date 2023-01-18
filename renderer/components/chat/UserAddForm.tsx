440205125343f76a047553d25bbc5bc318603b5a
Seungyeon Lee <sie9412@gmail.com>
2023-01-19 00:34:29 +0900

    fix: remove unused var

--- renderer/components/chat/UserAddForm.tsx
+++ renderer/components/chat/UserAddForm.tsx
@@ -1,6 +1,6 @@
 import { styled } from "@mui/material";
 
-const UserAddForm = styled("form")(({ theme }) => ({
+const UserAddForm = styled("form")(() => ({
   display: "flex",
   paddingTop: 10,
   paddingLeft: 10,
