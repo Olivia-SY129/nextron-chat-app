8d3578ed750bd79532d372a7c4a7e5ed8d0cab66
Seungyeon Lee <sie9412@gmail.com>
2023-01-18 19:31:28 +0900

    init

---
+++ renderer/lib/theme.ts
@@ -0,0 +1,20 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+      light: "#6a80e6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;
---
+++ renderer/pages/home.tsx
@@ -0,0 +1,28 @@
+import { styled } from "@mui/material";
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Layout from "../components/common/Layout";
+
+const Root = styled("div")(() => ({
+  textAlign: "center",
+}));
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>채팅앱 - 홈</title>
+      </Head>
+      <Layout>
+        <Root>
+          <h3>Welcome to Chat App</h3>
+          <p>Start chatting with your friends</p>
+          <Link href="/chat-room/1">단체 채팅방</Link>
+        </Root>
+      </Layout>
+    </React.Fragment>
+  );
+}
+
+export default Home;
