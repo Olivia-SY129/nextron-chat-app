94cd40438025c7383243a34b9f03cacf34b28f5f
Seungyeon Lee <sie9412@gmail.com>
2023-01-18 20:21:57 +0900

    fix: env config

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,7 +1,18 @@
+const process = require("process");
+
 module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+  },
+
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.target = 'electron-renderer';
+      config.target = "electron-renderer";
     }
 
     return config;
